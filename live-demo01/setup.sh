822c5aaea0ae55e9d2ff6dc4c4a1fc8bbaa1a2f4
Matt Gushee <matt@gushee.net>
2013-09-22 18:06:41 -0600

    Added test to ensure target dir is provided as argument.

--- live-demo01/setup.sh
+++ live-demo01/setup.sh
@@ -2,6 +2,11 @@
 
 demo_path="$1"
 
+if [ -z "$demo_path" ]; then
+    echo "USAGE: setup-sh <target_directory>"
+    exit 1
+fi
+
 mkdir -p ${demo_path}/data
 cp -R dynamic $demo_path
 csc live-demo01.scm
